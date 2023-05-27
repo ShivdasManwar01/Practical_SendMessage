e1b2e00bd4649fdfa4500979c2b21ff0fe225653
shivdas-manwar <shivdasmanwar2001@gmail.com>
2023-05-27 20:26:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sendmessage/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.sendmessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mess,phone;
+    Button sendbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mess=findViewById(R.id.mess);
+        phone=findViewById(R.id.phone);
+        sendbtn=findViewById(R.id.sendbtn);
+        sendbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent=new Intent(Intent.ACTION_VIEW);
+//                intent.putExtra("address",new String[]{phone.getText().toString()});
+//                intent.putExtra("message",mess.getText().toString());
+//                intent.setType("vnd.android-dir/mms-sms");
+                try {
+                    SmsManager smsManager=SmsManager.getDefault();
+                    smsManager.sendTextMessage(phone.getText().toString(),null,mess.getText().toString(),null,null);
+                    Toast.makeText(MainActivity.this, "message send succesfully", Toast.LENGTH_SHORT).show();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "message could not be sent", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
